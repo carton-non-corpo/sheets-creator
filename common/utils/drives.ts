2fd2cc8b2a6a43db1177049fdee288174cd7d713
Guillaume <guillaume@oss.ventures>
2025-08-29 14:37:23 +0200

    feat(drives): implement about 100 magic the gathering drives

--- common/utils/drives.ts
+++ common/utils/drives.ts
@@ -1,6 +1,121 @@
 import { Game, type GameFolders } from "../types/games";
 
 const mtgFolders: GameFolders[] = [
+  // By Main Groups
+  {
+    id: "1HPuw0u6y0cvbId0JT8wqLfZp0sjdfbkc",
+    name: "Planechase",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1kCGCpmT8kzeLr-hQFcst3j8xRz_TKaVR",
+    name: "White",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1eeV8ZcKxdI0cmpPaiNUtTVDZ30Wqyv8J",
+    name: "Blue",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1TLvAHhoLl9qsX5LjcCFl6cTwLy0JLaau",
+    name: "Black",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1Vg54l0e1e2VP87pzISnNJB4cTIwqfS-R",
+    name: "Red",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1v0VgXpHLUiPEcDr-ltUV7ThgCdHRut_5",
+    name: "Green",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1VpBej2ooDWNPrVMEW8UnVDqm6qQprY33",
+    name: "Multicolors",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1rWbDlQTiMNl3G4dzcLPI6VX4Sa0QRdwC",
+    name: "Colorless",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1AZMJ7qKi_FaENH-eTxjov1N75Ux2e3vD",
+    name: "Lands",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1PjofSLw0K1LCC36RtpnIQGd57qF_4lsp",
+    name: "Full Art Basic Lands",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1xY498X6PLCf7J-6Xyh9oVQSff49ilrMC",
+    name: "Normal Style Basic Lands",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1wvmV0zVn1Yann69IkBikkKJMcJyKZdYX",
+    name: "Retro Stryle Basic Lands",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1yOs420Hl5CkaNe3RzyOAVSYz9_FpCbor",
+    name: "Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+
+  // Crossovers
+  {
+    id: "16uXFPWJL67tFqBaarwMCEZxVovlFx1jd",
+    name: "Standalone Crossovers",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1TRIuroBR8AMlYKOS_vLHMhqoTsmNtlvU",
+    name: "Crossover: Dark Souls",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "15H5IwBV5NYchHNFPRYSO9m2IRCfHsuNy",
+    name: "Crossover: Godzilla",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1-MNFrsCsbviQMAuUfiAKjKa138GncsMS",
+    name: "Crossover: Heroes Of The Realm",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1vHwLxtkP2UU6zfS0UGkcU40ZBGvtsqzh",
+    name: "Crossover: The Walking Dead",
+    bleed: true,
+    game: Game.MTG
+  },
+
+
+
+  // Customs
   {
     id: "1EAqEy5Hpb-DTOj1nUdwxHpnrBx7MexXH",
     name: "Esper - Y'shtola Spellslinger",
@@ -11,6 +126,528 @@ const mtgFolders: GameFolders[] = [
     id: "1ZC81zbzZHThYNDMlPR0GO6csTnKKsGLd",
     name: "Proxy Adventure Time",
     bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "10LAS_3BlgVIjmuWw1RN5TDcK4AccMJvH",
+    name: "Arcane",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1QFLDSUQ-NV3ZYKirwDcCdvd-6VTe5T3o",
+    name: "Arcane Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "13rhBRrqMGnHTckGHHUdCM3zRCFar27UC",
+    name: "Avatar The Last Airbender - Customs",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "144pOlly71z5aa3_WkIKuclhoGBUFTj1D",
+    name: "Avatar The Last Airbender - Tokens - Customs",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1I4WTf20zaf1Xq9A_k3tVYQNrERrRotcr",
+    name: "Batman",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1Mr608-vhpqHOr5k14odjfw-M6xhhE_Da",
+    name: "Batman - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1a4IB00QmWECR4xy6dey29WdrSkkr7GAg",
+    name: "Berserk - Griffith",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1KOIYdGe3EoBSs3tG2L_ZfgDU1_jz9CNH",
+    name: "Berserk - Guts",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "LV6AHdXTSJgY_e9_TjWh4ueDhOsgJJVg",
+    name: "Berserk - Guts #2",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1jYBe5sANsuJbHM1pxPDSgRUyga7sGCJ9",
+    name: "Berserk - Guts #3",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "133wggfTLPe5slsmuYKxPQfX2z7HQQFGo",
+    name: "Big Tech",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "16KApTcRd9gJ7OpWisBwgofYsE_xeEELi",
+    name: "Bloodborn",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "110b-T27pUkas9ZJNhdh2R9M-SfUKyy_x",
+    name: "Bloodborn - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1GwNUABZGFotDccurP9cTwXsKsaqWuFrS",
+    name: "Bloodborn - Umbris, Fear Manifest",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1TwhKrWSDotMwfIwemz8IZK_NtRM6wKEO",
+    name: "Borderlands Proxy Deck",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1Ki6OKJhEAPwhs-E7mLZ0VCQyygKT2G3k",
+    name: "Borderlands Proxy Deck - Lands",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "18MUCkTl40RWYHk1fYajR9FmBJXW-Ywzu",
+    name: "Borderlands Proxy Deck - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1P70jmUD4a14NAv5PnlFBNn086kAV_FgM",
+    name: "Cats",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1PtGqVYyOjSAcwXnETLDLdn2kUfzO76Ex",
+    name: "Cyberpunk 2077",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1FJIDWORrDqToWMpPrpj-mEscLj9Ojqcm",
+    name: "Dark Souls Isshin",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1IS7HNR93QJxYe1py79P1HEhvi4clJ9Y-",
+    name: "Dark Souls Isshin - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1VOyCYB3dQTX7ijIcLS_Oba9FcfEXHQfo",
+    name: "Diablo",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1tfCQtu6PkD5L8ge46UWNftqQGqCEffx7",
+    name: "Diablo - Extras",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1no5ne9s_THaryjvqCVT_zhWpIO4dxaKS",
+    name: "Dr. Seuss",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1rNwrrwVL7kDUSAB19y8TUplH4ldnefhF",
+    name: "Demon Slayer",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1X1f25yY4febXVGWvs7ZvZDQF14UGS-ua",
+    name: "Demon Slayer - Front and Back cards",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1tBw6Fv5RS83dXIIARksJppGVl0QaCcZ-",
+    name: "Demon Slayer - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1mPkPZOlXiES55mIevyCelNmQHz0hIWFI",
+    name: "Dragon Quest",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1IofMWzXTAG0FskxMHaFtI5nTQ7Rwx1Kh",
+    name: "Dune - Rise of Muah'Dib",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1INwGpfgHY_862E_k4tItd5KJyNswjrva",
+    name: "Dune - Rise of Muah'Dib - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1-brEJjHJRhiUKjQxt4YycEQVtTRBlb-U",
+    name: "Dune - Baron",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1eX2orjmYduRbAsP_6OGOw9WNhs4llrce",
+    name: "Dune - Baron - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1zMXFrV7G_3jMDtBEX22QEyqkYtsf_x7P",
+    name: "Evangelion Neon Genesis Engine",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1gleqe-oh3aMIZIloCqvJQwZnPQoBqyaJ",
+    name: "Evangelion Neon Genesis Engine Revamped",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1JmoMR1vXjZJ5Ye9Hi72Zz2CUDAP5pVV-",
+    name: "Final Fantasy VII - Customs",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1k34aBmpRhiLHeFHtp4rwaQGwg0wGJ5YF",
+    name: "Backs and Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1gDxdyD-AARSA2w_aPwhGItuV5bkDKRhC",
+    name: "Full Metal Alchemist",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1OBesFl_TvabfHDDfu5NNjw58i_J_yYyu",
+    name: "Futurama",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1T3v-x6PNG25u5XDz5yn7FiOLGKy36Gsh",
+    name: "Gundam",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1YA9FCy-OzMRshLatfnad4CI9lK6bpruT",
+    name: "Game Of Thrones",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1ZaqHs2r36pjRgL9SzWVCT3gSsWJFGk1R",
+    name: "Goosebumps",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1koBmSc7U1Xlt8Qrz89HMEqfDVIPQ7Pfp",
+    name: "Gravity Falls",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1R4f_2Yfp8R3wranN2ZdUgqWzF6HZAELX",
+    name: "Gravity Falls - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1_TE_QAwBZ5c9-qAH16_QienBIBTUmdX2",
+    name: "Halo",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1gSMDHjxxCe3yTqPZipH4pI6IJugoF0oD",
+    name: "Horizon",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1ZaqHs2r36pjRgL9SzWVCT3gSsWJFGk1R",
+    name: "Junji Ito",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1QnrtLFx2rdblrwMwyu97B1f6GGfFCfgE",
+    name: "Kingdom Hearts",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1u1Juh4MqO7gE72xj2ik4-neecTXL5h2t",
+    name: "Lost",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "11R7G80oxlX5W4jVR83TzG5tIpNg1TOSwt",
+    name: "Lovecraft",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1PuClUs53kMIm3Ar6w70N7QuaDHngMIzW",
+    name: "Marvel - Customs",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1_F4AxBDX79Eut7XXm2tEHckfOPpLYAjY",
+    name: "Marvel - Customs - Lands",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1JlFvcw11aeQ5T7iExjWt2uL4L-y_he2V",
+    name: "Marvel - Customs - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1sczhh_R0I4ftNbZZ8uUuL7jGzQ8NOWK5",
+    name: "Monster Hunter",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1U7mTLnUqCrQ_0QNUDgFpmdRsRGKYaV-M",
+    name: "Mass Effect",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1--yuNwnUtYBcKQpGqKz-wn7qK-nKdGP_",
+    name: "Metroid",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1Scvrk710igoxdgwOzSAt6e5yEDLecuSU",
+    name: "Metroid - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1szyDwnhoDczhWH_ysGs7y806qhd5yKf6",
+    name: "Naruto",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1X1_ZL9vIJRppZTJnSVERatER6ImxYXBO",
+    name: "No Man's Sky",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1ldV0zoPbUDvlvruKOz84tg21w8SXkAJ2",
+    name: "No Man's Sky - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1emWiQ4aUSAFCXeVDIBhuSfX5H5YEr1m7",
+    name: "Pokemon(Wayta)",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1g5AJE3na_66iVhSOTsdGSPIawbWgOUy3",
+    name: "Pokemon(Wayta) - Lands",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1gFhcc1D84nzd3p1hoXzu_mYY39jxG5kn",
+    name: "Pokemon(Wayta) - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1ySHUyDshmx-hMINUnuc5SOg2G4mufVB3",
+    name: "Pokemon(TCG)",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1-BUEaURvMKbrg3zG-8L6s9MLED-NLYKL",
+    name: "One Piece",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1TXLRbPgTG1aC8ydnOmOA2zQ9vzy58G46",
+    name: "Over The Garden Wall",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1nKoYGiDzUT1S2X7MvgfNHRuXvvHMZt3n",
+    name: "Over The Garden Wall - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1WZO_LxyhCgdMRL6Wn93aFNsVa68T_7xb",
+    name: "Pastel Goth Spider",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1uLjUSI9xvVTmuULsbKgofkEGAR94mrJv",
+    name: "Sailor Moon",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1bv8MQvInvtKV1EGfVGIQGkONcOY6rqjk",
+    name: "Simpsons",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1niE0gAJCn-8KK673Hv1Pdf4a2FRQfmV-",
+    name: "Power Rangers",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1k-cSQO-sXNsxop4QbWMrfUA5x9t2OAkH",
+    name: "Power Rangers - Sideboard",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1Udxpf6VZ06b5d1hD3boMR_5CQ4pMWiXw",
+    name: "Power Rangers - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1NgfIwS-1MPpmL63D2W3W20Q3T3qHq0Ei",
+    name: "Star Wars",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1qFcKeyjK3sQW68nEiIPMFziarU-tkcAh",
+    name: "Star Wars Separatists",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1TvbK2AiB3RnguRuaFd7OExQulkv-wGkJ",
+    name: "Star Wars Separatists - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1QPyd6Bz5tKHB79_HsYiN0Y5SRS5UrfUg",
+    name: "Scooby-Doo",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1IqlRc_AFprQgA9kVxAbQp_izvRlfJ-cc",
+    name: "Scooby-Doo - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1ySYtsOskDLPd5mGrxiqeiBMb9s6vQIvl",
+    name: "Solo Leveling",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1e7UAImMfIydQmRDZeVxcv6eGTCfbNPMG",
+    name: "Solo Leveling - Full Arts",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1i_x3SAHWy7MtxKUYIQ4jRY5vxWDoy3K-",
+    name: "Solo Leveling - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1IOO0r4ZNzai9acDoi82x5wcxL-_mJ1zo",
+    name: "Spongebob",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1g2WL28BRIf5D9r-ST0qrKkzU0bv-0a99",
+    name: "Star Guardians - LoL",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1iByFHeIvdA2BMYib-G9wXF9MZXomtniD",
+    name: "Star Guardians - LoL - Alternatives",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1V4erF7flOMB0_ThlaodwuHxLlp6S4DRQ",
+    name: "Star Guardians - LoL - Tokens",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "15-uQBsM-81s1fKTKTFjD4mzejCGDhBGY",
+    name: "Ninja Turtles",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1HGEpkDRz14bSdiszKZjSY5sENuG5SKMw",
+    name: "Zelda - Hyrule's Arsenal",
+    bleed: true,
+    game: Game.MTG
+  },
+  {
+    id: "1uBXgLRHC51No6yqtYu2eqfoD3Ha-9xBD",
+    name: "Zelda - Lore",
+    bleed: false,
     game: Game.MTG
   },
 ]
