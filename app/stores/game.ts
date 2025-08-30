71d3ffbda91a00475613f918764937516b2c34b4
Guillaume <guillaume@oss.ventures>
2025-08-30 11:25:24 +0200

    feat(games): remember in cookies last selected game

--- app/stores/game.ts
+++ app/stores/game.ts
@@ -1,10 +1,20 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed } from 'vue'
 import { Game } from '~~/common/types/games'
 import { gameFolders } from '~~/common/utils/drives'
 
 export const useGameStore = defineStore('game', () => {
-  const selectedGame = ref<Game>(Game.MTG)
+  // Use cookie for SSR-safe persistence without blink
+  const selectedGameCookie = useCookie<Game>('selectedGame', {
+    default: () => Game.MTG
+  })
+
+  const selectedGame = computed({
+    get: () => selectedGameCookie.value,
+    set: (value: Game) => {
+      selectedGameCookie.value = value
+    }
+  })
 
   const currentGameFolders = computed(() => {
     return gameFolders.filter(folder => folder.game === selectedGame.value)
